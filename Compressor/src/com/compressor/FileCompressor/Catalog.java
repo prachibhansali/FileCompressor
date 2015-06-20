7f1055fe04e8416e96fca5ff74934aa5fb45c363
Prachi Bhansali <prachibh@ccs.neu.edu>
2015-06-19 20:29:24 -0400

    Reads a catalog file to read an index. Compresses the index once the index has been read. Performs encoding and decoding based on fixed length encoding of ascii

---
+++ Compressor/src/com/compressor/FileCompressor/Catalog.java
@@ -0,0 +1,83 @@
+package com.compressor.FileCompressor;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
+
+public class Catalog {
+	private HashMap<Integer,Tuple> catalog;
+	
+	public Catalog()
+	{
+		catalog = new HashMap<Integer,Tuple>();
+	}
+	
+	public void add(int termID,long offset,long size)
+	{
+		catalog.put(termID, new Tuple(offset,size));
+	}
+	
+	public Tuple getTupleAtIndex(int index)
+	{
+		return catalog.get(index);
+	}
+	
+	public void printToFile(String filename) throws FileNotFoundException
+	{
+		PrintWriter pw = new PrintWriter(filename);
+		Iterator<Integer> itr = getTermKeySet().iterator();
+		while(itr.hasNext()){
+			int termid = (int)itr.next();
+			pw.println(termid+" "+catalog.get(termid).toString());
+			
+		}
+		pw.close();
+	}
+	
+	public Catalog loadFromFile(String filename) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		String line = "";
+		while((line=br.readLine())!=null)
+		{
+			String[] parts = line.split(" ");
+			if(parts.length<=2) {
+				br.close();
+				throw new Exception("Incorrect format of file");
+			}
+			Tuple t = new Tuple(Long.parseLong(parts[1]),Long.parseLong(parts[2]));
+			catalog.put(Integer.parseInt(parts[0]),t);
+		}
+		br.close();
+		return this;
+	}
+	
+	public long size(){
+		return catalog.size();
+	}
+	
+	public Set<Integer> getTermKeySet()
+	{
+		return catalog.keySet();
+	}
+	
+	public boolean contains(int termID)
+	{
+		return catalog.containsKey(termID);
+	}
+
+	public Tuple get(int termID) {
+		return catalog.get(termID);
+	}
+
+	public void deleteID(int id) {
+		catalog.remove(id);
+	}
+}
---
+++ Compressor/src/com/compressor/FileCompressor/Tuple.java
@@ -0,0 +1,62 @@
+package com.compressor.FileCompressor;
+
+public class Tuple {
+	//int termID;
+	private long offset;
+	private long size;
+	
+	public Tuple(long offset,long size)
+	{
+		//setTermID(termid);
+		setOffset(offset);
+		setSize(size);
+	}
+	
+	/*public int getTermID()
+	{
+		return termID;
+	}
+	
+	public void setTermID(int termid)
+	{
+		termID=termid;
+	}
+	*/
+	
+	public long getOffset()
+	{
+		return offset;
+	}
+
+	public long getSize()
+	{
+		return size;
+	}
+	
+	public void setOffset(long offset)
+	{
+		this.offset=offset;
+	}
+	
+	public void setSize(long size)
+	{
+		this.size=size;
+	}
+	
+	public void setTuple(long offset,long size)
+	{
+		//setTermID(termid);
+		setOffset(offset);
+		setSize(size);
+	}
+	
+	public Tuple getTuple()
+	{
+		return this;
+	}
+	
+	public String toString()
+	{
+		return new String(offset+" "+size);
+	}
+}
